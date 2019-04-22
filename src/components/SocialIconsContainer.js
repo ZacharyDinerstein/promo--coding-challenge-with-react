f7192fd4dfdc844ae11cc48fe6e1ddbc3bcc0051
ZackDinerstein <zackdhome@gmail.com>
2019-04-22 18:20:03 +0300

    Added SocialIconsContainer

---
+++ src/components/SocialIconsContainer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export function SocialIconsContainer(props) {
+
+  return (
+    <div className="social-icons">
+	    <div className="social-icons__icon-container">
+	      <img src="https://ak02-cdn.slidely.com/static/junior-fed-test/Google_2015_logo_small.png" alt="" className="social-icons__icon" />
+	    </div>
+	    <div className="social-icons__icon-container">
+	      <img src="https://ak03-cdn.slidely.com/static/junior-fed-test/facebook-logo-small.png" alt="" className="social-icons__icon" />
+	    </div>
+	    <div className="social-icons__icon-container">
+	      <img src="https://ak04-cdn.slidely.com/static/junior-fed-test/Tesla-logo-small.png" alt="" className="social-icons__icon" />
+	    </div>
+	    <div className="social-icons__icon-container">
+	      <img src="https://ak01-cdn.slidely.com/static/junior-fed-test/nike_small.png" alt="" className="social-icons__icon" />
+	    </div>
+	</div>
+  );
+}
+
+
+
+		
+
