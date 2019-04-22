d71d2bbc751f3efc9a3359e5bd94a665019fe48e
ZackDinerstein <zackdhome@gmail.com>
2019-04-22 19:43:45 +0300

    editing

--- src/components/App.js
+++ src/components/App.js
@@ -7,23 +7,23 @@ import logo from '../logo.svg';
 import '../scss/App.scss';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      socialIconImgs: ['https://ak02-cdn.slidely.com/static/junior-fed-test/Google_2015_logo_small.png', 'https://ak03-cdn.slidely.com/static/junior-fed-test/facebook-logo-small.png', 'https://ak04-cdn.slidely.com/static/junior-fed-test/Tesla-logo-small.png', 'https://ak01-cdn.slidely.com/static/junior-fed-test/nike_small.png']
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			socialIconImgs: ['https://ak02-cdn.slidely.com/static/junior-fed-test/Google_2015_logo_small.png', 'https://ak03-cdn.slidely.com/static/junior-fed-test/facebook-logo-small.png', 'https://ak04-cdn.slidely.com/static/junior-fed-test/Tesla-logo-small.png', 'https://ak01-cdn.slidely.com/static/junior-fed-test/nike_small.png']
+		};
+	}
 
-  render() {
-    return (
-      <div>
-        <VideoComponent />
-        <Modal />
-        <Main socialIconImgs={this.state.socialIconImgs} />
-        <script src="main.js"></script>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<VideoComponent />
+				<Modal />
+				<Main socialIconImgs={this.state.socialIconImgs} />
+				<script src="main.js"></script>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { SocialIconsContainer } from './SocialIconsContainer.js';
-import { headlines } from './headlines.js';
+import { Headlines } from './Headlines.js';
+import { ButtonCallToAction } from './ButtonCallToAction.js';
 
 export class Main extends React.Component {
 	render(){
 		return (
 			<div>	
-		    <main>
-		      <Headlines />
-		      <ButtonCallToAction />
-		      <SocialIconsContainer socialIconImg={this.props.socialIconImgs} />
-		    </main>
-		</div>
+				<main>
+					<Headlines />
+					<ButtonCallToAction />
+					<SocialIconsContainer socialIconImg={this.props.socialIconImgs} />
+				</main>
+			</div>
 		);
 	}
 }
