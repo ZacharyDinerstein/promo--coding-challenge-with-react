1bc340984d59805686442dbd5c32a2504935a726
ZackDinerstein <zackdhome@gmail.com>
2019-04-22 18:47:54 +0300

    Create social icons logic

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,13 @@ import logo from '../logo.svg';
 import '../scss/App.scss';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      socialIconImgs: ['https://ak02-cdn.slidely.com/static/junior-fed-test/Google_2015_logo_small.png', 'https://ak03-cdn.slidely.com/static/junior-fed-test/facebook-logo-small.png', 'https://ak04-cdn.slidely.com/static/junior-fed-test/Tesla-logo-small.png', 'https://ak01-cdn.slidely.com/static/junior-fed-test/nike_small.png']
+    };
+  }
+
   render() {
     return (
       <div>
--- src/components/SocialIconsContainer.js
+++ src/components/SocialIconsContainer.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export function SocialIconsContainer(props) {
+export class SocialIconsContainer extends React.Component {
 
-  return (
-    <div className="social-icons">
-	    <div className="social-icons__icon-container">
-	      <img src="https://ak02-cdn.slidely.com/static/junior-fed-test/Google_2015_logo_small.png" alt="" className="social-icons__icon" />
-	    </div>
-	    <div className="social-icons__icon-container">
-	      <img src="https://ak03-cdn.slidely.com/static/junior-fed-test/facebook-logo-small.png" alt="" className="social-icons__icon" />
-	    </div>
-	    <div className="social-icons__icon-container">
-	      <img src="https://ak04-cdn.slidely.com/static/junior-fed-test/Tesla-logo-small.png" alt="" className="social-icons__icon" />
-	    </div>
-	    <div className="social-icons__icon-container">
-	      <img src="https://ak01-cdn.slidely.com/static/junior-fed-test/nike_small.png" alt="" className="social-icons__icon" />
-	    </div>
-	</div>
-  );
+	renderIcon(){
+		return (
+			<div className="social-icons__icon-container">
+				<img src="" alt="" className="social-icons__icon" />
+			</div>
+		);
+	}
+
+	renderIcons(){
+		let iconNum = 40;
+		let icons = [];
+
+		for (let i = 0; i < iconNum; i++){
+			icons.push(this.renderIcon())
+		}
+
+		return (
+			icons
+		)
+	}
+
+	render() {
+		return (
+			<div className="social-icons">{this.renderIcons()}</div>
+		);
+	}
 }
-
-
-
-		
-
